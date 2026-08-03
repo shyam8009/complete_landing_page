e967d4e98b6d31d2157aebb212d89d37191ab41b
Shyam Bhadja <shyam8009@github.com>
2026-08-03 06:44:53 +0530

    Fix layout crash on 3D Drone Detector page caused by invalid imports

--- src/pages/3DDetectionRadar/ThreeDDetectionRadarPage.tsx
+++ src/pages/3DDetectionRadar/ThreeDDetectionRadarPage.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Navbar } from '../../components/Navbar';
-import { Footer } from '../../components/Footer';
 import { HeroSection } from './components/HeroSection';
 import { PipelineSection } from './components/PipelineSection';
 import { ValuePropositionSection } from './components/ValuePropositionSection';
@@ -10,19 +8,13 @@ import { ClosingSection } from './components/ClosingSection';
 
 export function ThreeDDetectionRadarPage() {
   return (
-    <div className="min-h-screen bg-[#000000] text-white selection:bg-[#84CC16] selection:text-black font-sans">
-      <Navbar />
-      
-      <main className="w-full flex flex-col pt-20">
-        <HeroSection />
-        <PipelineSection />
-        <ValuePropositionSection />
-        <SubsystemsSection />
-        <UseCasesSection />
-        <ClosingSection />
-      </main>
-
-      <Footer />
+    <div className="w-full bg-[#000000] text-white selection:bg-[#84CC16] selection:text-black font-sans overflow-hidden">
+      <HeroSection />
+      <PipelineSection />
+      <ValuePropositionSection />
+      <SubsystemsSection />
+      <UseCasesSection />
+      <ClosingSection />
     </div>
   );
 }
