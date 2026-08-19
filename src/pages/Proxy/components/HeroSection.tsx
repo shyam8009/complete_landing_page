473420ddbcfecd2579867f067172811b8cf187ce
Shyam Bhadja <shyam8009@github.com>
2026-08-19 17:05:17 +0530

    chore: clean duplicate Tailwind class tokens in 22 hero content containers

--- src/pages/Proxy/components/HeroSection.tsx
+++ src/pages/Proxy/components/HeroSection.tsx
@@ -33,7 +33,7 @@ export function HeroSection() {
       <div className="absolute inset-0 bg-black/20 z-10" />
       
       {/* Content Container */}
-      <div className="relative z-20 flex-1 flex flex-col justify-center pt-20 sm:pt-24 md:pt-20 sm:pt-24 md:pt-28 pb-8 sm:pb-10 md:pb-12 w-full px-4 sm:px-4 sm:px-6 lg:px-12 max-w-[1600px] mx-auto">
+      <div className="relative z-20 flex-1 flex flex-col justify-center pt-20 sm:pt-24 md:pt-28 pb-8 sm:pb-10 md:pb-12 w-full px-4 sm:px-6 lg:px-12 max-w-[1600px] mx-auto">
         <div className="max-w-3xl">
           
           {/* Eyebrow */}
@@ -84,6 +84,7 @@ export function HeroSection() {
     </section>
   );
 }
+
 
 
 
