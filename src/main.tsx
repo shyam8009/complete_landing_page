bb18f19312092ca130914ae0c275f11cc85119f7
Shyam Bhadja <shyam8009@github.com>
2026-09-08 16:50:55 +0530

    fix(routing): switch to HashRouter for reliable SPA navigation on cPanel

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 
   import { createRoot } from "react-dom/client";
-  import { BrowserRouter } from "react-router";
+  import { HashRouter } from "react-router";
   import App from "./app/App.tsx";
   import { ErrorBoundary } from "./components/ErrorBoundary.tsx";
   import "./styles/index.css";
@@ -11,9 +11,9 @@
 
   createRoot(document.getElementById("root")!).render(
     <ErrorBoundary>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ErrorBoundary>
   );
   
